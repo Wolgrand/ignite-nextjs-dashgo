6921efffed23255431cee26a971a018ec21319d2
Wolgrand Neto <wolgrand@live.com>
2021-04-07 19:41:01 -0300

    Add Create User Page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,150 @@
+import {Flex, Heading, Icon, Table, Button,Box,Text, Stack, SimpleGrid, theme, Thead, Tr, Th, Checkbox, Tbody, Td} from '@chakra-ui/react'
+import dynamic from 'next/dynamic';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import {Header} from '../../components/Header'
+import { Pagination } from '../../components/Pagination';
+import { Sidebar } from '../../components/Sidebar';
+
+export default function UserList(){
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p="8"
+        >
+          <Flex
+            mb="8"
+            justify="space-between"
+            align="center"
+          >
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+            >
+              Criar novo
+            </Button>
+
+          </Flex>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink"/>
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"/>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold" >Wolgrand Neto</Text>
+                    <Text fontSize="sm" color="gray.300" >wolgrand@email.com</Text>
+                  </Box>
+                </Td>
+                <Td>07 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"/>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold" >Wolgrand Neto</Text>
+                    <Text fontSize="sm" color="gray.300" >wolgrand@email.com</Text>
+                  </Box>
+                </Td>
+                <Td>07 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"/>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold" >Wolgrand Neto</Text>
+                    <Text fontSize="sm" color="gray.300" >wolgrand@email.com</Text>
+                  </Box>
+                </Td>
+                <Td>07 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"/>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold" >Wolgrand Neto</Text>
+                    <Text fontSize="sm" color="gray.300" >wolgrand@email.com</Text>
+                  </Box>
+                </Td>
+                <Td>07 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
